65efaef612bbb9f9a78da1308f537558307ef26d
michaelyin <gt8442b@yahoo.com>
2019-04-22 23:05:13 -0400

    curl testing shell scripts.

---
+++ curl_test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+curl  -F 'image=@./02264090.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02263227.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./0252ir.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02261092.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02261509.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02260745.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02262875.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02263620.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02263989.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02261607.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./0722aat.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02261832.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02260601.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02260148.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02260739.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02264061.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02264074.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./0706aad.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02260725.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./0686zj.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
+curl  -F 'image=@./02264054.jpg'  -F 'id=999' http://localhost:8686/answer/blank | jq .
